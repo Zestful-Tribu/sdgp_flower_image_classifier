1659aef76441b0ca880f88da1e0a119f05455788
charani0618 <cvidarsha@gmail.com>
2021-04-19 09:38:13 -0700

    first five flowers  added

--- FlowerUse.py
+++ FlowerUse.py
@@ -3,46 +3,46 @@
 flowerInfo = [
     {
         'name':'anthurium',
-        'medical':"",
+        'medical':"It is useful for muscle crams",
         'cosmetic':"",
         'edibility':"",
-        'decorative':"",
+        'decorative':"used for decorative occasions",
+        'e-benefit':"They are one of the best houseplants that purify indoor air"
+
+    },
+    {
+        'name':'bellflower',
+        'medical':"used in traditional Chinese medicine,In China they are used as a cough suppressant and expectorant for common colds cough sore throat tonsillitis, and chest congestion,giving the best treatment to skin problems while removing artificial scents and colourants from the products. Consumers are being more aware of products",
+        'cosmetic':"Removes artificial scents and artificial colourants from products,Its very good for skin",
+        'edibility':"These flowers are edible,it makes an interesting addition to summer salads,the flowers can also be used as decoration in salads or atop cakes and other desserts",
+        'decorative':"These can be used for garden decoration,It is useful for cake decorations and food decorations",
+        'e-benefit':"These can be used for garden decoration,These flowers attract a lot of bees"
+
+    },
+    {
+        'name':'cannas',
+        'medical':"Relief of fever,Laxative urine,Lowering blood pressurev,Antiviral,Antibacterial,Antithrombotic,As protection from damage caused by UV radiation,Antialergenik",
+        'cosmetic':"",
+        'edibility':"Natural food coloring,Natural preservative,",
+        'decorative':"Used to tie beads and rosaries and to make rattles,sometimes used as an ornamental plant in the yard of our house",
         'e-benefit':""
 
     },
     {
-        'name':'anthurium',
-        'medical':"",
+        'name':'spider flower',
+        'medical':"It is useful for treat fervers, scorpion stings and arthritis,The seeds of spider flower contain oil that can be extracted and used for medicinal purposes",
         'cosmetic':"",
-        'edibility':"",
-        'decorative':"",
+        'edibility':"spider flower contains hydrocyanic acid, which is removed with cooking",
+        'decorative':"spider flower is cultivated in household gardens as a valuable potherb or for animal fodder",
         'e-benefit':""
 
     },
     {
-        'name':'anthurium',
-        'medical':"",
-        'cosmetic':"",
-        'edibility':"",
-        'decorative':"",
-        'e-benefit':""
-
-    },
-    {
-        'name':'anthurium',
-        'medical':"",
-        'cosmetic':"",
-        'edibility':"",
-        'decorative':"",
-        'e-benefit':""
-
-    },
-    {
-        'name':'anthurium',
-        'medical':"",
-        'cosmetic':"",
-        'edibility':"",
-        'decorative':"",
+        'name':'iris',
+        'medical':"sometimes used in aromatherapy as sedative medicines",
+        'cosmetic':"Used for perfumes,Used for natural toothpaste",
+        'edibility':"These are suitable for drinking",
+        'decorative':"Used for wedding decorations",
         'e-benefit':""
 
     },
